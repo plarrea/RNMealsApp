76ca0e462dacb4661edbf01b0fdb5bf87158d29a
Pablo Larrea <larrea.pablo@gmail.com>
2026-02-05 16:41:11 -0500

    update main navigation

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StatusBar, StyleSheet } from 'react-native';
+import CategoriesScreen from './src/screens/CategoriesScreen';
+import MealDetailScreen from './src/screens/MealDetailScreen';
+import MealsOverviewScreen from './src/screens/MealsOverviewScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="light" />
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="MealsCategories"
+          screenOptions={{
+            headerTintColor: 'white',
+            headerStyle: { backgroundColor: '#351401' },
+            contentStyle: { backgroundColor: '#3f2f25' },
+          }}
+        >
+          <Stack.Screen
+            name="MealsCategories"
+            component={CategoriesScreen}
+            options={{ title: 'All Categories' }}
+          />
+          <Stack.Screen
+            name="MealsOverview"
+            component={MealsOverviewScreen}
+            // options={({ route, _ }) => {
+            //   const categoryName = route.params.categoryName;
+            //   return {
+            //     titile: categoryName,
+            //   };
+            // }}
+          />
+          <Stack.Screen name="MealDetail" component={MealDetailScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container: { flex: 1, backgroundColor: '#24180f' },
 });
