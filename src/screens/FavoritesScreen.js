0976f563f6198d341f49014c4cd789052bf91636
Pablo Larrea <larrea.pablo@gmail.com>
2026-02-06 10:57:01 -0500

    update favorites screen

--- src/screens/FavoritesScreen.js
+++ src/screens/FavoritesScreen.js
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native';
 import MealsList from '../components/MealsList/MealsList';
 import { MEALS } from '../data/dummy-data';
-import { useFavorites } from '../store/context/use-favorites';
+// import { useFavorites } from '../store/context/use-favorites';
+import { useSelector } from 'react-redux';
 
 const FavoritesScreen = () => {
-  const { ids } = useFavorites();
+  const ids = useSelector((state) => state.favoriteMeals.ids);
+  // const { ids } = useFavorites();
   const meals = MEALS.filter((meal) => ids.includes(meal.id));
 
   if (meals.length === 0) {
